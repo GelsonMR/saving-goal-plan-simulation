fa5224e9ea5033e90398448206901eab93d5afa0
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-11 12:44:42 -0300

    Added Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './styles';
+import { Header } from './components';
 
 const App: React.FunctionComponent = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
     </ThemeProvider>
   );
 };
