7101d13fccf0858f130af77e529f6cfd9677f885
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-11 16:13:41 -0300

    Added CSS reset for margin, padding and border

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  *, *::before, *::after: {
+  *, *::before, *::after {
     box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
   }
 
   body {
