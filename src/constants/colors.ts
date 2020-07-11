92d2fd84f4807a316da049eaf32ddaaaabbb39d0
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-11 11:38:02 -0300

    Added color constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  almostBlack: '#1C1E1F',
+  prettyGray: '#657786',
+  gray: '#E1E8ED',
+  lightGray: '#E1E8ED',
+  lighterGray: '#F2F2F2',
+  blue: '#0079FF',
+  darkBlue: '#1B31A8'
+};
