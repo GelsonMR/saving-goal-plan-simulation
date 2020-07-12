1eb87aceaa755756178237c449c5c13bf0617567
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-11 21:58:08 -0300

    Updated logo import

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, theme } from './styles';
 import { Header } from './components';
-import Logo from './icons/logo.svg';
 import { SavingGoalScreen } from './screens';
 
 const App: React.FunctionComponent = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Header logo={Logo} logoAlt="Origin" />
+      <Header logo="./icons/logo.svg" logoAlt="Origin" />
       <SavingGoalScreen />
     </ThemeProvider>
   );
