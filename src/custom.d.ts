c583f1a161402b8ce336835340fb78d5850381a6
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-11 21:57:15 -0300

    Added DefaulTheme props

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,13 @@
+import 'styled-components';
+
 declare module '*.svg' {
   const content: string;
   export default content;
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    fontFamily: string;
+    backgroundColor: string;
+  }
 }
