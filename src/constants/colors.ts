b0909f02c7cd8eef3c31abc456da73b130b55b96
Gelson Mariano Rodrigues <gelson.rodrigues@luizalabs.com>
2020-07-12 21:01:12 -0300

    Updated colors to match mockups

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,8 +2,8 @@ const colors = {
   almostBlack: '#1C1E1F',
   prettyGray: '#657786',
   gray: '#E1E8ED',
-  lightGray: '#E1E8ED',
-  lighterGray: '#F2F2F2',
+  lightGray: '#F2F2F2',
+  lighterGray: '#F4F8FA',
   blue: '#0079FF',
   darkBlue: '#1B31A8'
 };
