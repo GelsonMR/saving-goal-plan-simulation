c74ed37c195ae857afd78f0ce22f442e4e080d71
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2020-07-16 15:24:48 -0300

    Updates

---
+++ precache-manifest.71386851879dd0e26bac6fc369ea0f73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781dd447becf9baeb7fa564e7d1b0fe9",
+    "url": "/saving-goal-plan-simulation/index.html"
+  },
+  {
+    "revision": "754d811a67d310147af5",
+    "url": "/saving-goal-plan-simulation/static/css/main.6b142cf3.chunk.css"
+  },
+  {
+    "revision": "5d30d2175c4a1d2c29e9",
+    "url": "/saving-goal-plan-simulation/static/js/2.8bf02735.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saving-goal-plan-simulation/static/js/2.8bf02735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754d811a67d310147af5",
+    "url": "/saving-goal-plan-simulation/static/js/main.4dd043c4.chunk.js"
+  },
+  {
+    "revision": "d91e2e28ff44f95432be",
+    "url": "/saving-goal-plan-simulation/static/js/runtime-main.30b6cdfa.js"
+  },
+  {
+    "revision": "6a23a8a65b6226a4871fcf3903dbcf4e",
+    "url": "/saving-goal-plan-simulation/static/media/arrow.6a23a8a6.svg"
+  },
+  {
+    "revision": "889409eaf7fad74d43c91dc0d5f9c2ca",
+    "url": "/saving-goal-plan-simulation/static/media/house.889409ea.svg"
+  },
+  {
+    "revision": "c84bf198c0793ac78614278351968028",
+    "url": "/saving-goal-plan-simulation/static/media/logo.c84bf198.svg"
+  }
+]);
